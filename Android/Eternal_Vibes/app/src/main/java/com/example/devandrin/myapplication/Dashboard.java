526763f4cb629d45dddee5cf8aa5ab2ea80a92b4
Devandrin Kuni <201320596@student.uj.ac.za>
2017-03-14 21:42:29 +0200

    Basic View, working window switching between dashboard and login

--- Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/Dashboard.java
+++ Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/Dashboard.java
@@ -1,13 +1,28 @@
 package com.example.devandrin.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
-
+    private Button loginb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        loginb = (Button)findViewById(R.id.btnLogin);
+        loginb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+    private void Login()
+    {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
