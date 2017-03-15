6879d2c79a23405b7b7fe92436cfb80afcb7213d
Devandrin Kuni <201320596@student.uj.ac.za>
2017-03-15 12:02:42 +0200

    Window Switching

--- Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/Dashboard.java
+++ Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/Dashboard.java
@@ -1,32 +1,57 @@
 package com.example.devandrin.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.content.SharedPreferences.Editor;
 public class Dashboard extends AppCompatActivity {
     private Button loginb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        loginb = (Button)findViewById(R.id.btnStart);
-        loginb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Go();
-            }
-        });
+        if(seenPage())
+        {
+            nextActivity();
+        }else
+        {
+            loginb = (Button)findViewById(R.id.btnStart);
+            loginb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Go();
+                }
+            });
+        }
+
     }
     private void Go()
     {
+        SharedPreferences sp = this.getSharedPreferences(getString(R.string.seenDash),Context.MODE_PRIVATE);
+        SharedPreferences.Editor e = sp.edit();
+        e.putBoolean(getString(R.string.seenDash),true);
+        e.commit();
         Toast t = Toast.makeText(getApplicationContext(),"Woop Woop!",Toast.LENGTH_LONG);
         t.show();
+        nextActivity();
+    }
+    private void nextActivity()
+    {
         Intent i = new Intent(this,HomeActivity.class);
         startActivity(i);
+        finish();
+    }
 
+    private boolean seenPage()
+    {
+        SharedPreferences sp = this.getSharedPreferences(getString(R.string.seenDash),Context.MODE_PRIVATE);
+        boolean value = false;
+        boolean isSeen = sp.getBoolean(getString(R.string.seenDash),value);
+        return isSeen;
     }
 }
--- Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/HomeActivity.java
+++ Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.example.devandrin.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -47,8 +50,6 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
         }
     }
 
@@ -84,8 +85,16 @@ public class HomeActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_logout) {
-
+        } else if (id == R.id.nav_logout)
+        {
+            SharedPreferences sp = this.getSharedPreferences(getString(R.string.seenDash),Context.MODE_PRIVATE);
+            SharedPreferences.Editor e = sp.edit();
+            e.putBoolean(getString(R.string.seenDash),false);
+            e.commit();
+            Intent i = getBaseContext().getPackageManager()
+                    .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
         } else if (id == R.id.nav_exit) {
 
         }
