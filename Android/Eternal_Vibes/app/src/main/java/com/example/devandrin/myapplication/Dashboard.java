8b156c520232a0ee36c1c6d4522ef215f8713b58
Devandrin Kuni <201320596@student.uj.ac.za>
2017-03-15 10:52:53 +0200

    Working layout

--- Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/Dashboard.java
+++ Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/Dashboard.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
     private Button loginb;
@@ -12,17 +13,20 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        loginb = (Button)findViewById(R.id.btnLogin);
+        loginb = (Button)findViewById(R.id.btnStart);
         loginb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Login();
+                Go();
             }
         });
     }
-    private void Login()
+    private void Go()
     {
-        Intent intent = new Intent(this,LoginActivity.class);
-        startActivity(intent);
+        Toast t = Toast.makeText(getApplicationContext(),"Woop Woop!",Toast.LENGTH_LONG);
+        t.show();
+        Intent i = new Intent(this,HomeActivity.class);
+        startActivity(i);
+
     }
 }
