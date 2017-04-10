969b519887ddf383b9702a4ebdb441baf467e3c9
Devandrin Kuni <201320596@student.uj.ac.za>
2017-04-10 16:55:56 +0200

    Messenger Implementation

--- Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/HomeActivity.java
+++ Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/HomeActivity.java
@@ -4,12 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.view.MotionEvent;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -18,11 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TableLayout;
-import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity   implements NavigationView.OnNavigationItemSelectedListener {
-    private float x1,x2;
     private  static HomeActivity instance = null;
 
     public static HomeActivity getInstance() {
@@ -32,11 +25,10 @@ public class HomeActivity extends AppCompatActivity   implements NavigationView.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        instance = this;
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        instance = this;
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -57,6 +49,7 @@ public class HomeActivity extends AppCompatActivity   implements NavigationView.
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
         viewPager.setCurrentItem(1);
+
     }
 
     @Override
