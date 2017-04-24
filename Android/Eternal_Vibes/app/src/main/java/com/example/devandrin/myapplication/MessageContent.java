40bc610a8bcdbd9b5743155e755b86aca010eeec
Ronald <201433999@student.uj.ac.za>
2017-04-24 21:12:06 +0200

    Add files via upload
    I implemented a listview for the radar and chat.

---
+++ Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/MessageContent.java
@@ -0,0 +1,39 @@
+package com.example.devandrin.myapplication;
+
+/**
+ * Created by Ronnie on 2017-04-24.
+ */
+
+public class MessageContent
+{
+    private String sYourMessage;
+    private String sReplyMessage;
+
+    public MessageContent(String sYourMessage, String sReplyMessage)
+    {
+        this.sYourMessage = sYourMessage;
+        this.sReplyMessage = sReplyMessage;
+    }
+
+    //Getters and Setters
+
+    public String getsYourMessage()
+    {
+        return sYourMessage;
+    }
+
+    public void setsYourMessage(String sYourMessage)
+    {
+        this.sYourMessage = sYourMessage;
+    }
+
+    public String getsReplyMessage()
+    {
+        return sReplyMessage;
+    }
+
+    public void setsReplyMessage(String sReplyMessage)
+    {
+        this.sReplyMessage = sReplyMessage;
+    }
+}
