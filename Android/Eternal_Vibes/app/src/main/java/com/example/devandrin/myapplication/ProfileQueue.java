4c9355c8b0b52396d0f447c01717504c64e8c89e
Devandrin Kuni <201320596@student.uj.ac.za>
2017-07-26 19:01:34 +0200

    Revert "Revert "Pull request for site""

---
+++ Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/ProfileQueue.java
@@ -0,0 +1,262 @@
+package com.example.devandrin.myapplication;
+
+/**
+ * Created by Ronald on 7/14/2017.
+ */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ProfileQueue implements Iterable<RadarContent> {
+
+    private RadarContent[] arrProfiles;
+    private int length = 0;
+
+    /**
+     * Create a brand new Priority Queue to hold future profiles
+     */
+    public ProfileQueue() {
+        this.arrProfiles = new RadarContent[0];
+        this.length = 0;
+    }
+
+    /**
+     * Create a new Priority Queue using an existing array of user profiles
+     * @param arrProfiles - Array of profiles obtained from the API response
+     */
+    public ProfileQueue(RadarContent[] arrProfiles) {
+        this.arrProfiles = arrProfiles;
+        this.length = arrProfiles.length;
+    }
+
+    /**
+     * Create a new Priority Queue using an array list and converting them to an array for optimal sort performance
+     * @param radarContentList
+     */
+    public ProfileQueue(ArrayList<RadarContent> radarContentList){
+        this.arrProfiles = new RadarContent[radarContentList.size()];
+        radarContentList.toArray(arrProfiles);
+        this.length = radarContentList.size();
+    }
+
+    /**
+     * Invokes the relevant sorting functions based on user selection (Still need to adds this functionality)
+     * @param sortType - Specifies what the Profiles will be sorted by
+     * @param isAscending - Smallest to largest or vice versa
+     */
+    public void ProfileSort(String sortType, Boolean isAscending) {
+        if(sortType.equals("DISTANCE")) {
+            //Sort by distance
+            quickSortDistance(arrProfiles, 0, arrProfiles.length - 1, isAscending);
+        }
+        else {
+            //Sort by rating
+            quickSortRating(arrProfiles, 0, arrProfiles.length - 1, isAscending);
+        }
+    }
+
+    /**
+     * Sort by Distance
+     * @param arrProfiles
+     * @param low - Start value
+     * @param high - Max length of the array
+     * @param isAscending - Sort by smallest to largest or vice versa
+     */
+    private void quickSortDistance(RadarContent[] arrProfiles, int low, int high, Boolean isAscending) {
+        if (low >= high)
+            return;
+
+        // pick the pivot
+        int middle = low + (high - low) / 2;
+        int pivot = arrProfiles[middle].getDistance();
+
+        // make left < pivot and right > pivot
+        int i = low, j = high;
+        while (i <= j) {
+
+            if(isAscending == true) {
+                while (arrProfiles[i].getDistance() < pivot) {
+                    i++;
+                }
+
+                while (arrProfiles[j].getDistance() > pivot) {
+                    j--;
+                }
+            }
+            else //Sort by largest value to smallest
+            {
+                while (arrProfiles[i].getDistance() > pivot) {
+                    i++;
+                }
+
+                while (arrProfiles[j].getDistance() < pivot) {
+                    j--;
+                }
+            }
+
+            if (i <= j) {
+                //Swap the profiles based on distance
+                RadarContent temp = arrProfiles[i];
+                arrProfiles[i] = arrProfiles[j];
+                arrProfiles[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // recursively sort two sub parts of the array of profiles
+        if (low < j)
+            quickSortDistance(arrProfiles, low, j, isAscending);
+
+        if (high > i)
+            quickSortDistance(arrProfiles, i, high, isAscending);
+    }
+
+    /**
+     * Sort by Rating
+     * @param arrProfiles
+     * @param low - Start value
+     * @param high - Max length of the array
+     * @param isAscending - Sort by smallest to largest or vice versa
+     */
+    private void quickSortRating(RadarContent[] arrProfiles, int low, int high, Boolean isAscending) {
+        if (low >= high)
+            return;
+
+        // pick the pivot
+        int middle = low + (high - low) / 2;
+        int pivot = arrProfiles[middle].getRating();
+
+        // make left < pivot and right > pivot
+        int i = low, j = high;
+        while (i <= j) {
+
+            //Sort by smallest value to largest
+            if(isAscending == true) {
+                while (arrProfiles[i].getRating() < pivot) {
+                    i++;
+                }
+
+                while (arrProfiles[j].getRating() > pivot) {
+                    j--;
+                }
+            }
+            else //Sort by largest value to smallest
+            {
+                while (arrProfiles[i].getRating() > pivot) {
+                    i++;
+                }
+
+                while (arrProfiles[j].getRating() < pivot) {
+                    j--;
+                }
+            }
+
+            if (i <= j) {
+                //Swap the profiles based on rating
+                RadarContent temp = arrProfiles[i];
+                arrProfiles[i] = arrProfiles[j];
+                arrProfiles[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // recursively sort two sub parts of the array of profiles
+        if (low < j)
+            quickSortRating(arrProfiles, low, j, isAscending);
+
+        if (high > i)
+            quickSortRating(arrProfiles, i, high, isAscending);
+    }
+
+    /**
+     * Shift the elements in the array and decrease its size after each removal of a profile
+     */
+    private void shiftElements() {
+        RadarContent[] tempArr = new RadarContent[length - 1];
+        for(int i = 0; i < length - 1; i++) {
+            tempArr[i] = arrProfiles[i + 1];
+        }
+        arrProfiles = tempArr;
+        length--;
+    }
+
+    /**
+     * @return and remove the Profile with the highest priority from the Priorty Queue
+     */
+    public RadarContent poll() {
+        RadarContent p = arrProfiles[0];
+        shiftElements();
+        return p;
+    }
+
+    /**
+     * @return the Profile with the highest priority without removing it from the Priority Queue
+     */
+    public RadarContent peek() {
+        return arrProfiles[0];
+    }
+
+    private void expandArray() {
+        RadarContent[] expandedArray = new RadarContent[this.length + 1];
+
+        for(int i = 0; i < length; i++) {
+            expandedArray[i] = arrProfiles[i];
+        }
+
+        arrProfiles = expandedArray;
+        length++;
+    }
+
+    /**
+     * Expands the array and adds a Profile to end of the Priority Queue
+     * @param p - A profile
+     */
+    public void add(RadarContent p) {
+        expandArray();
+        arrProfiles[length - 1] = p;
+    }
+
+    /**
+     * @return check if the Priority Queue is empty
+     */
+    public Boolean isEmpty() {
+        return length == 0;
+    }
+
+    /**
+     * Iterator to go through each Profile for this data structure
+     */
+    @Override
+    public Iterator iterator() {
+
+        Iterator<RadarContent> iterator = new Iterator<RadarContent>() {
+
+            ProfileQueue pq = new ProfileQueue(arrProfiles);
+            int iCursor;
+
+            /*
+             * Check for next available profile
+             */
+            @Override
+            public boolean hasNext() {
+                return !pq.isEmpty() && pq.arrProfiles[iCursor++] != null;
+            }
+
+            /**
+             * Return the next profile and advance the cursor
+             */
+            @Override
+            public RadarContent next() {
+                RadarContent p = pq.arrProfiles[iCursor];
+                iCursor++;
+                return p;
+            }
+        };
+
+        return iterator;
+
+    } //end of method
+
+} //end of class
--- Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/RadarUtil.java
+++ Android/Eternal_Vibes/app/src/main/java/com/example/devandrin/myapplication/RadarUtil.java
@@ -1,11 +1,14 @@
 package com.example.devandrin.myapplication;
 
+import android.preference.ListPreference;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Devandrin on 2017/04/01.
@@ -13,22 +16,82 @@ import java.util.ArrayList;
 
 public class RadarUtil extends Content {
 
+    //Use two different arraylists to prevent clashing
+    private static ArrayList<RadarContent> unsorted_radarList;
+    private static ArrayList<RadarContent> sorted_radarList;
+    private RadarContent[] arrAPI_Profiles = new RadarContent[5];
+    private static RadarAdapter raObj;
+    private static View view;
+
+    /**
+     * @param inflater - Inflates and creates the required xml files
+     * @param container - Holds a bunch of views together
+     */
     public RadarUtil(LayoutInflater inflater, ViewGroup container) {
         super(inflater, container);
     }
-    private static ArrayList<RadarContent> radarList;
+
+    /**
+     * @return a custom view to display content
+     */
     @Override
     public View displayContent() {
-        View view = super.displayContent();
-         radarList = new ArrayList<>();
+        view = inflater.inflate(R.layout.list_fragment, container, false);
 
-        radarList.add(new RadarContent("Bob", "8km", "Sandton", 500));
-        radarList.add(new RadarContent("Tom", "5km", "Randburg", 1500));
-        radarList.add(new RadarContent("Jane", "21km", "Rosebank", 850));
+        setupProfiles();
+        UpdatedSort_RadarProfiles("DISTANCE", false); //Default sort by shortest distance
 
-        RadarAdapter raObj = new RadarAdapter(HomeActivity.getInstance().getApplicationContext(), radarList);
-        ListView lv = (ListView) view.findViewById(R.id.ArrayList);
-        lv.setAdapter(raObj);
         return view;
     }
-}
+
+    /**
+     * Sends a request and receives an array of profiles from the API response
+     * Then calculates the distance of each user to check if they are within range
+     */
+    private void setupProfiles(){
+        //Dummy profiles are used for now until the API to return actual profiles is implemented
+        arrAPI_Profiles[0] = new RadarContent(452, "Jessica Grom", 12, 3, "Intermediate", "Sandton", 2100);
+        arrAPI_Profiles[1] = new RadarContent(89, "Bob Brown", 20, 2, "Beginner", "Houghton", 500);
+        arrAPI_Profiles[2] = new RadarContent(1552, "Tom Yeis", 18, 4, "Advanced", "Randburg", 1500);
+        arrAPI_Profiles[3] = new RadarContent(452, "Tiffany Vlein", 40, 5, "Master", "Pretoria", 4100);
+        arrAPI_Profiles[4] = new RadarContent(842, "Jerry Alko", 34, 1, "Beginner", "Soweto", 800);
+
+        //Calculate distance will be added soon
+
+        //Each time the items in the Radar are sorted it must do it with a brand new list and not an existing one
+        unsorted_radarList = new ArrayList<>();
+        for(int i = 0; i < arrAPI_Profiles.length; i++){
+            if(arrAPI_Profiles[i].getDistance() <= 45) {
+                unsorted_radarList.add(arrAPI_Profiles[i]);
+            }
+        }
+    }
+
+    public static void UpdatedSort_RadarProfiles(String RadarSortType, Boolean isAscending){
+
+        ProfileQueue pqObj = new ProfileQueue(unsorted_radarList);
+        pqObj.ProfileSort(RadarSortType, isAscending);
+
+        //Each time the items in the Radar are sorted,
+        //it must do it with a brand new list and not an existing one
+        sorted_radarList = new ArrayList<>();
+
+        while(pqObj.iterator().hasNext()){
+            sorted_radarList.add(pqObj.poll());
+            //Remove each Profile from the queue with the highest priority then
+            //add each profile to an arraylist so it can displayed in the RadarAdapter class
+        }
+
+        setupRadar(view);
+    }
+
+    //Setup the variables to display the resulting sorted profiles
+    private static void setupRadar(View view){
+
+        raObj = new RadarAdapter(HomeActivity.getInstance().getApplicationContext(), sorted_radarList);
+        ListView lv = (ListView) view.findViewById(R.id.ArrayList);
+        lv.setAdapter(raObj);
+        raObj.notifyDataSetChanged();
+    }
+
+}//end of class
